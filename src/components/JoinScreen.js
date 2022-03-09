cab73c079c459ee325c3fa2ef72744a3f4177ce1
stporter <stporter2@gmail.com>
2022-03-09 16:54:59 -0500

    cannot figure out why questions are wrong when I know they are correct and why timer is not working

---
+++ src/components/JoinScreen.js
@@ -0,0 +1,11 @@
+function JoinScreen({ start }) {
+	return (
+		<div className='join-screen'>
+			<h2>Friends Trivia</h2>
+			<p>Good Luck!</p>
+			<button onClick={start}>Start</button>
+		</div>
+	);
+}
+
+export default JoinScreen;
--- src/components/QuizScreen.js
+++ src/components/QuizScreen.js
@@ -4,34 +4,31 @@ import { useState } from 'react';
 import QuizResult from './QuizResult.js';
 import Question from './Question.js';
 
-function QuizScreen({retry}) {
+function QuizScreen({ retry }) {
 	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 	const [markedAnswers, setMarkedAnswers] = useState(
 		new Array(QuestionList.length)
 	);
 	const isQuestionEnd = currentQuestionIndex === QuestionList.length;
 
-function calculateResult(){
-    let correct = 0;
-    QuestionList.forEach((question,index)=>{
-if(question.correctOptionIndex == markedAnswers[index]){
-    correct++;
-}
-    });
-    return {
-      total:QuestionList.length,
-      correct:correct,
-      percentage:Math.trunc(correct / QuestionList.length) * 100) 
-    };
-}
+	function calculateResult() {
+		let correct = 0;
+		QuestionList.forEach((question, index) => {
+			if (question.correctOptionIndex == markedAnswers[index]) {
+				correct++;
+			}
+		});
+		return {
+			total: QuestionList.length,
+			correct: correct,
+			percentage: Math.trunc((correct / QuestionList.length) * 100),
+		};
+	}
 
 	return (
 		<div className='quiz-screen'>
 			{isQuestionEnd ? (
-				<QuizResult 
-                result={calculateResult()}
-                retry={retry}
-                />
+				<QuizResult result={calculateResult()} retry={retry} />
 			) : (
 				<Question
 					question={QuestionList[currentQuestionIndex]}
